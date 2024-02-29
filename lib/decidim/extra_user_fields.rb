9517905551332c4145796bc21ef5e035f99176b4
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2024-02-29 11:39:49 +0100

    feat: allow to configure notifications_sending_frequency

--- lib/decidim/extra_user_fields.rb
+++ lib/decidim/extra_user_fields.rb
@@ -8,5 +8,10 @@ require "decidim/extra_user_fields/form_builder_methods"
 module Decidim
   # This namespace holds the logic of the `ExtraUserFields` module.
   module ExtraUserFields
+    include ActiveSupport::Configurable
+
+    config_accessor :notifications_sending_frequency do
+      ENV.fetch("NOTIFICATIONS_SENDING_FREQUENCY", "daily")
+    end
   end
 end
